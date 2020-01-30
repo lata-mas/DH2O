d402c77eed4ab4e86a42621ad20ee833ba240059
JulioLanda <landajulioc@gmail.com>
2020-01-30 12:56:37 -0600

    Primera versión funcional del código

---
+++ wifi.py
@@ -0,0 +1,16 @@
+def do_connect(red,clave):
+    import network
+    import time
+    sta_if = network.WLAN(network.STA_IF)
+    if not sta_if.isconnected():
+        print('connecting to network...')
+        sta_if.active(True)
+	sta_if.connect(red,clave)
+        sta_if.connect('IER', 'acadier2014')
+        while not sta_if.isconnected():
+            pass
+    else:
+      print("Already connected")
+    print('network config:', sta_if.ifconfig())
+    time.sleep(5)
+
