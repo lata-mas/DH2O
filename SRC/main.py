3e5e370a5406a4b17fea327261a13d250e514e3f
altamarmx <gbv@ier.unam.mx>
2021-05-21 15:46:00 -0500

    reduje el c'odigo de main8s y lo pas'e a main.py

---
+++ SRC/main.py
@@ -0,0 +1,100 @@
+
+print("hola 8")
+print("Se han habilitado 8 sensores")
+print("Pines empleados: sensor1------pin 5"
+                       "sensor2------pin 4"
+                       "sensor3------pin  0"
+                       "sensor4------pin  2"
+                       "sensor5------pin 14"
+                       "sensor6------pin 12"
+                       "sensor7------pin 13"
+                       "sensor8------pin  3")
+
+from funciones import *       #funciones para hacer el código modular
+from wifi import do_connect   #importa función para conexión wifi de wifi.py
+
+#importar librerías
+import time
+#import utime               ++++++++++++
+#import network             ++++++++++++
+#import sys                 ++++++++++++
+
+
+#variables para los intentos de conexión
+cnt_boot = 0
+cont = 0
+
+#configuración del network
+#sta_if = network.WLAN(network.STA_IF)        ++++++++++++
+#sta_if.active(True)                          ++++++++++++
+
+#Conexión a internet
+#ap_if = network.WLAN(network.AP_IF)         ++++++++++++
+#sta_if.connect(red,clave)                   ++++++++++++
+
+
+#Segundos entre persona para publicar
+seg = 10
+Sensor1 = Conteo(pin=5,seg=seg,k=0.003125,label='Litros 1')
+Sensor1.atributo()
+
+Sensor2 = Conteo(pin=4,seg=seg,k=0.003125,label='Litros 2')
+Sensor2.atributo()
+
+Sensor3 = Conteo(pin=0,seg=seg,k=0.003125,label='Litros 3')
+Sensor3.atributo()
+
+Sensor4 = Conteo(pin=2,seg=seg,k=0.003125,label='Litros 4')
+Sensor4.atributo()
+
+#Declaración del sensor5
+Sensor5 = Conteo(pin=14,seg=seg,k=0.003125,label='Litros 5')
+Sensor5.atributo()
+
+Sensor6 = Conteo(pin=12,seg=seg,k=0.003125,label='Litros 6')
+Sensor6.atributo()
+
+Sensor7 = Conteo(pin=13,seg=seg,k=0.003125,label='Litros 7')
+Sensor7.atributo()
+
+Sensor8 = Conteo(pin=3,seg=seg,k=0.003125,label='Litros 8')
+Sensor8.atributo()
+
+	#-------------------------Loop infinito----------------------------------
+	while True:
+		try:
+			#publicación de datos en thingsboard
+		    Sensor1.publica()
+		    Sensor2.publica()
+		    Sensor3.publica()
+		    Sensor4.publica()
+		    Sensor5.publica()
+		    Sensor6.publica()
+		    Sensor7.publica()
+		    Sensor8.publica()
+			#Reinicio de variables
+		    cnt_boot = 0
+		    cont = 1
+			#reintentar conexión en caso de fallo
+	  	except Exception as inst:
+	    	print(inst)
+	    	do_connect(red,clave);
+		    cnt_boot += 1
+		    cont += 1
+		    print("Fail {}".format(cnt_boot))
+	    	if cont >1:
+		        Sensor1.contador = Sensor1.contador
+		        Sensor2.contador = Sensor2.contador
+		        Sensor3.contador = Sensor3.contador
+		        Sensor4.contador = Sensor4.contador
+		        Sensor5.contador = Sensor5.contador
+		        Sensor6.contador = Sensor6.contador
+		        Sensor7.contador = Sensor7.contador
+		        Sensor8.contador = Sensor8.contador
+	    	if cnt_boot > 10: #si falla la reconexión mas de diez veces se reinicia el dispositivo
+	      		machine.reset()
+	    	time.sleep(1)
+
+
+			else:
+				print("Acción no válida")
